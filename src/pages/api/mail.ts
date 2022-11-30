1cdd5cb9759d01123baa0369076d874bf1453aa6
Slap-a-tha-Bass <cdeloach16@gmail.com>
2022-11-30 13:53:55 -0600

    fix: sendgrid

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,24 +1,29 @@
+import sendgrid from "@sendgrid/mail";
 import { NextApiRequest, NextApiResponse } from "next";
-const mail = require("@sendgrid/mail");
-mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 export default async function Mail(req: NextApiRequest, res: NextApiResponse) {
-  const { firstName, lastName, email, subject, message } = req.body;
-  const content = `
-    First Name: ${firstName}\r\n
-    Last Name: ${lastName}\r\n
-    Subject: ${subject}\r\n
-    Email: ${email}\r\n
-    Message: ${message}
-  `;
+  try {
+    const { firstName, lastName, email, subject, message } = req.body;
+    const content = `
+      First Name: ${firstName}\r\n
+      Last Name: ${lastName}\r\n
+      Subject: ${subject}\r\n
+      Email: ${email}\r\n
+      Message: ${message}
+    `;
 
-  const data = {
-    to: "yarar-fisher.1@osu.edu",
-    from: "hello@pro-sci.science",
-    subject: "New Message from Contact Form",
-    text: content,
-    html: content.replace(/\r\n/g, "<br>"),
-  };
-  await mail.send(data);
+    const data = {
+      to: "yarar-fisher.1@osu.edu",
+      from: "hello@pro-sci.science",
+      subject: "New Message from Contact Form",
+      text: content,
+      html: content.replace(/\r\n/g, "<br>"),
+    };
+    await sendgrid.send(data);
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
   res.status(200).json({ status: "OK" });
 }
