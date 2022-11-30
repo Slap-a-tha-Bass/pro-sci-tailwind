168987a96daab4eeef15e87bf09bd318e0e31f82
Slap-a-tha-Bass <cdeloach16@gmail.com>
2022-11-30 14:11:24 -0600

    build: google console

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,21 +1,24 @@
 export default function Head() {
-    return (
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="google" content="notranslate" />
-        <meta httpEquiv="Content-Language" content="en_US" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Pro-SCI - Spinal Cord Injury Research</title>
-        <meta
-          name="description"
-          content="Pro-SCI - Changing spinal cord injury care one piece of evidence at a time."
-        />
-        <meta name="author" content="Dr. Ceren Yarar-Fisher" />
-        <meta name="robots" content="index, follow" />
-        <meta name="googlebot" content="index, follow" />
-        <meta name="google" content="nositelinkssearchbox" />
-        <link rel="icon" href="/spinalcordinjury.png" />
-      </head>
-    );
-  }
-  
+  return (
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="google" content="notranslate" />
+      <meta httpEquiv="Content-Language" content="en_US" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Pro-SCI - Spinal Cord Injury Research</title>
+      <meta
+        name="description"
+        content="Pro-SCI - Changing spinal cord injury care one piece of evidence at a time."
+      />
+      <meta name="author" content="Dr. Ceren Yarar-Fisher" />
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
+      <meta name="google" content="nositelinkssearchbox" />
+      <meta
+        name="google-site-verification"
+        content="9cnudisQDKLbRM8sKJEmrzJjm56TIGDu_cu6HZfjgxw"
+      />
+      <link rel="icon" href="/spinalcordinjury.png" />
+    </head>
+  );
+}
