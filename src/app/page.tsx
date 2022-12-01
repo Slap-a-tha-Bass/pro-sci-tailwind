0a8951f271c61a0e3cd15be11cfcb24e60d88e88
Slap-a-tha-Bass <cdeloach16@gmail.com>
2022-12-01 08:42:20 -0600

    style: font sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
     <main className="relative grid grid-cols-1 h-full">
       <div className="flex justify-center items-start pt-4 px-4 sm:px-8">
         <div className="p-8 sm:p-16">
-          <ul className="text-xl sm:text-4xl text-white leading-relaxed">
+          <ul className="text-xl sm:text-3xl lg:text-4xl text-white leading-relaxed">
             <li>We are here to make</li>
             <li className="pt-2 ml-4">
               <span className="text-sky-700">pro-</span>
@@ -48,8 +48,8 @@ export default function HomePage() {
           </ul>
         </div>
       </div>
-      <div className="mx-auto -mt-4 bg-gray-900 p-6 rounded-lg z-10 max-w-xs h-56 sm:max-w-lg sm:h-full">
-        <div className="flex flex-col text-xl sm:text-3xl text-white px-10">
+      <div className="mx-auto -mt-8 bg-gray-900 pt-8 rounded-lg z-10 max-w-xs h-56 sm:max-w-lg sm:h-60">
+        <div className="flex flex-col text-xl sm:text-2xl text-white px-8">
           <p>Interested in collaborating or joining the team?</p>
           <Link
             href="/contact"
