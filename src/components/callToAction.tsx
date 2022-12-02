eba49b5e8688afa77f92df4fbe48b4c1126b8eae
Slap-a-tha-Bass <cdeloach16@gmail.com>
2022-12-02 11:58:56 -0600

    style: button cta

--- src/components/callToAction.tsx
+++ src/components/callToAction.tsx
@@ -12,12 +12,14 @@ export default function CallToAction({
     <div
       className={`mx-auto ${marginTop} ${marginBottom} bg-gray-900 pt-8 rounded-lg z-10 h-56 sm:max-w-max sm:max-h-max sm:h-60`}
     >
-      <div className="flex flex-col justify-center items-center text-xl sm:text-2xl text-white px-8">
-        <p className="mt-6">Collaborate and join our team!</p>
+      <div className="flex flex-col justify-center items-center text-white px-8">
+        <p className="mt-6  text-xl sm:text-3xl">
+          Collaborate and join our team!
+        </p>
         <Link
           href="/contact"
           aria-label="Contact"
-          className="flex mx-auto mt-8 w-32 rounded-xl justify-center items-center text-md p-2 bg-sky-700 shadow-white animate-pulse hover:bg-sky-900 hover:animate-none hover:shadow-whitelg"
+          className="flex mx-auto mt-8 w-32 rounded-xl justify-center items-center text-lg p-2 bg-sky-700 shadow-white animate-pulse hover:bg-sky-900 hover:animate-none hover:shadow-whitelg"
         >
           Contact <ArrowRight />
         </Link>
